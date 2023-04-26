6e1ab9ccc2a7f5ed4a502157f60c51ab1ba13cf0
Oleksandr <sbarsuk88@gmail.com>
2023-04-26 12:42:04 +0300

    added domain model (#6)

---
+++ src/backend/MessageService.Domain/Model/Message.cs
@@ -0,0 +1,21 @@
+﻿namespace MessageService.Domain.Model
+{
+    public class Message
+    {
+        public Guid Id { get; init; }
+
+        public int SenderId { get; init; }
+
+        public int ReceiverId { get; init; }
+
+        public string Text { get; init; }
+
+        public Message(int senderId, int receiverId, string text, Guid id = default)
+        {
+            Id = id;
+            SenderId = senderId;
+            ReceiverId = receiverId;
+            Text = text;
+        }
+    }
+}
