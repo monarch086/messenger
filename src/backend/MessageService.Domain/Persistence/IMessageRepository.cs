c5254f3dbdf6c33a364c4e5334b44c037583ad0c
Oleksandr Barsuk <sbarsuk88@gmail.com>
2023-04-26 12:24:47 +0300

    added domain model

---
+++ src/backend/MessageService.Domain/Persistence/IMessageRepository.cs
@@ -0,0 +1,13 @@
+﻿using MessageService.Domain.Model;
+
+namespace MessageService.Domain.Persistence
+{
+    public interface IMessageRepository
+    {
+        Task AddMessageAsync(int senderId, int receiverId, string text);
+
+        Task<IEnumerable<Message>> GetMessagesAsync(int userId, int friendId, DateTime from, DateTime till);
+
+        Task<IEnumerable<Message>> GetLatestMessagesAsync(int userId, int lastMessageId, int? friendId = null);
+    }
+}
