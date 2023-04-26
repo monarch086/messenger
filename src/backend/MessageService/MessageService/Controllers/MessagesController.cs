a4e226b844f3da5d47937c3f3e0891b715c70776
Oleksandr Barsuk <sbarsuk88@gmail.com>
2023-04-26 13:31:27 +0300

    updated solution structure

---
+++ src/backend/MessageService/MessageService/Controllers/MessagesController.cs
@@ -0,0 +1,33 @@
+﻿using MessageService.Domain.Model;
+using MessageService.Domain.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessageService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private IMessageRepository _messageRepository;
+
+        public MessagesController(IMessageRepository messageRepository)
+        {
+            _messageRepository = messageRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Message> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Message(index, index, $"text {index}"))
+            .ToArray();
+        }
+
+        [HttpPost]
+        public async Task<Message> Add(Message message)
+        {
+            var savedMessage = await _messageRepository.AddMessageAsync(message);
+
+            return savedMessage;
+        }
+    }
+}
