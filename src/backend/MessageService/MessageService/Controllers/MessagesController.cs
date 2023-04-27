ac411f29500b12c7f689505c1a3a0da396f4b4ff
Oleksandr Barsuk <sbarsuk88@gmail.com>
2023-04-27 10:09:12 +0300

    updated messages controller

--- src/backend/MessageService/MessageService/Controllers/MessagesController.cs
+++ src/backend/MessageService/MessageService/Controllers/MessagesController.cs
@@ -15,22 +15,34 @@ namespace MessageService.Controllers
             _messageRepository = messageRepository;
         }
 
-        [HttpGet]
-        public async Task<IEnumerable<Message>> Get()
-        {
-            var from = DateTime.Now - TimeSpan.FromDays(1);
-            var till = DateTime.Now + TimeSpan.FromDays(1);
-            var messages = await _messageRepository.GetMessagesAsync(1, 2, from, till);
-
-            return messages;
-        }
-
         [HttpPost]
         public async Task<Message> Add(Message message)
         {
             var savedMessage = await _messageRepository.AddMessageAsync(message);
 
             return savedMessage;
+        }
+
+        [HttpGet("/{userId}")]
+        public async Task<IEnumerable<Message>> Get(int userId, int friendId, DateTime from, DateTime till)
+        {
+            if (from == default || till == default)
+            {
+                from = DateTime.UtcNow - TimeSpan.FromDays(7);
+                till = DateTime.UtcNow;
+            }
+
+            var messages = await _messageRepository.GetMessagesAsync(userId, friendId, from, till);
+
+            return messages;
+        }
+
+        [HttpGet("latest/{userId}")]
+        public async Task<IEnumerable<Message>> GetLatest(int userId, Guid lastMessageId, int? friendId = null)
+        {
+            var messages = await _messageRepository.GetLatestMessagesAsync(userId, lastMessageId, friendId);
+
+            return messages;
         }
     }
 }
