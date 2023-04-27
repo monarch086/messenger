15447cb662625cd457205638a01aaad3f8393cf8
Oleksandr Barsuk <sbarsuk88@gmail.com>
2023-04-27 17:07:37 +0300

    setup load-balancer with message-service

---
+++ src/backend/MessageService/MessageService/Controllers/ServerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MessageService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ServerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var value = Environment.GetEnvironmentVariable("APPID");
+
+            return Ok($"Server {value}");
+        }
+    }
+}
